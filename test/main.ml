ffba169e5578cfcd7022bc76f6abb5bd7d73a949
Andrew Chiu <andrewchiu198@gmail.com>
2023-11-17 00:36:36 -0500

    Added Initial Bisect Coverage Framework

--- test/main.ml
+++ test/main.ml
@@ -1,6 +1,10 @@
-(* open Stocks *)
+open Stocks
 open OUnit2
 
-let this_suite = [ ("" >:: fun _ -> assert_equal true true) ]
+let user = ref (Cli.Cli.make_user "username" 0)
+
+let this_suite =
+  [ ("" >:: fun _ -> assert_equal 0 (Cli.Cli.view_balance !user)) ]
+
 let test_suite = "Test" >::: List.flatten [ this_suite ]
 let () = run_test_tt_main test_suite
